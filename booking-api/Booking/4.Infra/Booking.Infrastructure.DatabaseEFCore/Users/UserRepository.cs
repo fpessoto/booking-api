a483925ef0a99ded591f75cda3b062e0e8f43da1
fpessoto <felipe.pessoto@hotmail.com>
2022-09-27 08:51:09 -0300

    fixed repository

--- booking-api/Booking/4.Infra/Booking.Infrastructure.DatabaseEFCore/Users/UserRepository.cs
+++ booking-api/Booking/4.Infra/Booking.Infrastructure.DatabaseEFCore/Users/UserRepository.cs
@@ -51,12 +51,12 @@ namespace Booking.Infrastructure.DatabaseEFCore.Users
 
         public  async Task<User?> GetAsync(string email, string password)
         {
-            return await _context.Users.AsNoTracking().FirstAsync(u => u.Email == email && u.Password == password);
+            return await _context.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Email == email && u.Password == password);
         }
 
         public async Task<User?> GetAsync(string email)
         {
-            return await _context.Users.FirstAsync(u => u.Email == email);
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
         }
     }
 }
